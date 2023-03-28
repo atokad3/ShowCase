b0bd302d132f1e7c391540584d05776c72720bec
mechompchomp <120221314+mechompchomp9000@users.noreply.github.com>
2023-03-28 13:34:26 -0700

    haha color changes on change

--- Assets/Scripts/EnergyBar.cs
+++ Assets/Scripts/EnergyBar.cs
@@ -6,8 +6,8 @@ using UnityEngine.UI;
 public class EnergyBar : MonoBehaviour
 {
     public Slider energySlider;
-
-
+    public Gradient gradient;
+    public Image fill;
 
 
     // Start is called before the first frame update
@@ -26,11 +26,12 @@ public class EnergyBar : MonoBehaviour
     {
         energySlider.maxValue = energy;
         energySlider.value = energy;
-
+        fill.color = gradient.Evaluate(1f);
     }
 
     public void SetEnergy(int energy)
     {
         energySlider.value = energy;
+        fill.color = gradient.Evaluate(energySlider.normalizedValue);
     }
 }
